c52299514bce53288d143c5720c0d0b74dccd54c
Pl4inzz <mr.red208210@gmail.com>
2026-09-09 08:56:23 +0700

    tambah

--- resources/views/kategori/index.sakuci.php
+++ resources/views/kategori/index.sakuci.php
@@ -3,31 +3,37 @@
 @section ('content')
 
 <div class="container">
-    <h1>Daftar Kategori</h1>
-    <a href="{{ route('admin.kategori.create') }}" class="btn btn-primary mb-3">Tambah Kategori</a>
+    <h1>Data Kategori</h1>
+    <a href="{{ route('admin.kategori.create') }}" class="btn btn-primary mb-3 btn-sm">Tambah Kategori</a>
     <table class="table table-bordered table-striped">
         <thead>
             <tr>
-                <th>ID</th>
+                <th>No</th>
                 <th>Keterangan</th>
                 <th>Aksi</th>
             </tr>
         </thead>
         <tbody>
-            @php $no=1; @endphp
+            @php
+                $no = 1;
+            @endphp
             @foreach ($data as $kategori)
-            <tr>
-                <td>{{ $no++ }}</td>
-                <td>{{ $kategori->keterangan }}</td>
-                <td>
-                    <a href="{{ route('admin.kategori.edit', ['id_kategori' => $kategori->id_kategori]) }}" class="btn btn-success btn-sm">Edit</a>
-                    <a href="" class="btn btn-danger btn-sm">Delete</a>
-                </td>
-            </tr>
+                <tr>
+                    <td>{{ $no++ }}</td>
+                    <td>{{ $kategori->keterangan }}</td>
+                    <td>
+                        <a href="{{ route('admin.kategori.edit', ['id' => $kategori->id_kategori]) }}" class="btn btn-success btn-sm">Edit</a>
+                        <form action="{{ route('admin.kategori.delete', ['id' => $kategori->id_kategori]) }}" method="POST" style="display: inline-block;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus kategori ini?')">Hapus</button>
+                        </form>
+                    </td>
+    
+                </tr>
             @endforeach
         </tbody>
     </table>
-
     {!! $data->links() !!}
 </div>
 @endsection
